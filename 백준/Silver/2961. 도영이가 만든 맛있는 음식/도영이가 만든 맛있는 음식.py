21a3d325b30fe9af989d34765c120c218b36769b
Yeongjae-Kong <67358433+Yeongjae-Kong@users.noreply.github.com>
2025-08-11 12:06:15 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 60 ms, Memory: 36464 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from itertools import combinations
+import math
+
+n = int(input())
+s = []
+b = []
+for _ in range(n):
+    s1, b1 = map(int, input().split())
+    s.append(s1)
+    b.append(b1)
+
+dic = defaultdict(int)
+
+mul_s = []
+sum_b = []
+for i in range(1, n+1):
+    for a in combinations(s, i):
+        mul = 1
+        for j in a:
+            mul *= j
+            mul_s.append(mul)
+for i in range(1, n+1):
+    for a in combinations(b, i):
+        sum = 0
+        for j in a:
+            sum += j
+            sum_b.append(sum)
+            
+tmp = math.inf
+for i in range(len(mul_s)):
+    abs(mul_s[i] - sum_b[i])
+    if (tmp > abs(mul_s[i] - sum_b[i])):
+        tmp = abs(mul_s[i] - sum_b[i])
+print(tmp)
